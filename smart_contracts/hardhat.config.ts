c333496b3ac5cee9d5d549f0a2f07f7d6935c6e0
Stanislav <verbatim8872@gmail.com>
2024-02-13 00:12:47 +0200

    hardhat config

--- smart_contracts/hardhat.config.ts
+++ smart_contracts/hardhat.config.ts
@@ -1,15 +1,17 @@
+import { readFileSync } from "fs";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const apiKey = readFileSync(".api").toString();
+const account_private_key = readFileSync(".account_private_key").toString();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
     hardhat: {},
     sepolia: {
-      url: "https://sepolia.infura.io/v3/5861c02e289e496d8b0c5c317bcac25c",
-      accounts: [
-        "df0ff7b396453ecf5e9e8b55fcebaf32d3ba52892ca733a48c4f5d46fa4773d0",
-      ],
+      url: "https://sepolia.infura.io/v3/" + apiKey,
+      accounts: [account_private_key],
     },
   },
 };
