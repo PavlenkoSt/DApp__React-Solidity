acfbffde518a96c3f5dd1a8ea1ce2cc9c182c9ef
Stanislav <verbatim8872@gmail.com>
2024-02-13 21:20:15 +0200

    transaction contract logic, save addresses after deploy

--- smart_contracts/contracts/Transactions.sol
+++ smart_contracts/contracts/Transactions.sol
@@ -12,22 +12,17 @@ contract Transactions {
   }
 
   TransactionStruct[] transactions;
-  uint256 transactionCount;
 
   event Transfer(address from, address to, uint amount, string message, string keyword, uint256 timestamp);
 
-  function setTransaction (address payable to, uint amount, string memory message, string memory keyword) public {
-    transactionCount += 1;
-    transactions.push(TransactionStruct(msg.sender, to, amount, message, keyword, block.timestamp));
+  function setTransaction (address payable to, string memory message, string memory keyword) public payable {
+    transactions.push(TransactionStruct(msg.sender, to, msg.value, message, keyword, block.timestamp));
+    to.transfer(msg.value);
 
-    emit Transfer(msg.sender, to, amount, message, keyword, block.timestamp);
+    emit Transfer(msg.sender, to, msg.value, message, keyword, block.timestamp);
   }
 
   function getAllTransactions () public view returns (TransactionStruct[] memory) {
     return transactions;
-  }
-
-  function getTransactionCount () public view returns (uint256) {
-    return transactionCount;
   }
 }
--- smart_contracts/scripts/deploy.ts
+++ smart_contracts/scripts/deploy.ts
@@ -1,9 +1,22 @@
 import { ethers } from "hardhat";
+import fs from "fs/promises";
+
+async function saveDeployedAddress(json: string) {
+  await fs.writeFile("contractAddresses.json", json, { encoding: "utf-8" });
+}
 
 async function main() {
   const Transcations = await ethers.getContractFactory("Transactions");
   const transcations = await Transcations.deploy();
   await transcations.waitForDeployment();
+
+  const transactionsAddress = await transcations.getAddress();
+
+  const objToSave = {
+    transactions: transactionsAddress,
+  };
+
+  await saveDeployedAddress(JSON.stringify(objToSave));
 }
 
 main().catch((error) => {
